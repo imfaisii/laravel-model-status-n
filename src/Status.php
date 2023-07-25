ff6f3b14f01ca7b2822ffd6f396a14278bb67931
Faisal Ashfaq <59202459+imfaisii@users.noreply.github.com>
2023-07-25 19:41:49 +0500

    Initial commit

---
+++ src/Status.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Spatie\ModelStatus;
+
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphTo;
+
+class Status extends Model
+{
+    protected $guarded = [];
+
+    protected $table = 'statuses';
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function model(): MorphTo
+    {
+        return $this->morphTo();
+    }
+
+    public function __toString(): string
+    {
+        return $this->name;
+    }
+}
